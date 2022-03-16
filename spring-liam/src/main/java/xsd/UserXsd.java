1f209183b153c6da89f3c18e098b5ba104135015
liam <zhuchanglizq@126.com>
2022-03-16 18:18:24 +0800

    实现自定的标签

---
+++ spring-liam/src/main/java/xsd/UserXsd.java
@@ -0,0 +1,28 @@
+package xsd;
+
+/**
+ * @Description
+ * @Author liam661
+ * @Date 2022/3/16 11:14
+ **/
+public class UserXsd {
+	private String name;
+
+	private String idCard;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIdCard() {
+		return idCard;
+	}
+
+	public void setIdCard(String idCard) {
+		this.idCard = idCard;
+	}
+}
